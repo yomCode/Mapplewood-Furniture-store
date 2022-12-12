4b13944164ff6a593acb1193bf337dbb98003002
fortune <fortuneojih@yahoo.com>
2022-12-12 11:49:45 +0100

    Created a docket bean

--- src/main/java/com/decagon/OakLandv1be/OakLandV1BeApplication.java
+++ src/main/java/com/decagon/OakLandv1be/OakLandV1BeApplication.java
@@ -2,12 +2,21 @@ package com.decagon.OakLandv1be;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 public class OakLandV1BeApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(OakLandV1BeApplication.class, args);
 	}
-
+	@Bean
+	public Docket productApi(){
+		return new Docket(DocumentationType.SWAGGER_2).select().apis(RequestHandlerSelectors.basePackage("com.decagon.OakLandv1be")).build();
+	}
 }
