2fb86dc5d5db026ca35c04af7245350c8fb4417a
CourageOghogho <oghogho.courage@gmail.com>
2022-12-13 01:17:00 +0100

    tested dummy

---
+++ src/test/java/com/decagon/OakLandv1be/OakLandV1BeApplicationTest.java
@@ -0,0 +1,14 @@
+package com.decagon.OakLandv1be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+@SpringBootTest
+class OakLandV1BeApplicationTest {
+    @Test
+    public  void dummyTest(){
+        assertEquals(1,1);
+    }
+
+}
