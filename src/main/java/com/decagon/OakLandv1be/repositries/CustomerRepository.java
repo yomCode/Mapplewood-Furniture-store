fb91b3fe6537e2c98c7ffd0005292e9f4bb8dad9
Olivia-onyinye <n.oliviaonyinye@gmail.com>
2022-12-16 14:32:01 +0100

    corrected the dependencies

--- src/main/java/com/decagon/OakLandv1be/repositries/CustomerRepository.java
+++ src/main/java/com/decagon/OakLandv1be/repositries/CustomerRepository.java
@@ -6,5 +6,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
-    Boolean existsByEmail(String email);
+    //Boolean existsByEmail(String email);
 }
