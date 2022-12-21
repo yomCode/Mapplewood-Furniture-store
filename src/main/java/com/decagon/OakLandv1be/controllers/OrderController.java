aacbc16a38963304ae80c82ab85fe25f454bbfe5
fortune <fortuneojih@yahoo.com>
2022-12-21 09:03:13 +0100

    Checkout controller

--- src/main/java/com/decagon/OakLandv1be/controllers/OrderController.java
+++ src/main/java/com/decagon/OakLandv1be/controllers/OrderController.java
@@ -9,6 +9,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,7 +21,7 @@ public class OrderController {
     private final CheckoutService checkoutService;
 
     @GetMapping("/checkout")
-    public ResponseEntity<Order> checkoutOrder(AddressRequestDto addressRequestDto, CheckoutItemDto checkoutItemDto){
+    public ResponseEntity<Order> checkoutOrder(@RequestBody AddressRequestDto addressRequestDto, CheckoutItemDto checkoutItemDto){
         Order checkoutResponseDto=checkoutService.cartCheckout(addressRequestDto,checkoutItemDto);
         return new ResponseEntity<>(checkoutResponseDto, HttpStatus.OK);
     }
