3d897ad00249ca63567f79dfba2e940167a11c9f
favourdan <106870617+favourdan@users.noreply.github.com>
2022-12-21 10:28:00 +0100

    No changes

--- src/main/java/com/decagon/OakLandv1be/OakLandV1BeApplication.java
+++ src/main/java/com/decagon/OakLandv1be/OakLandV1BeApplication.java
@@ -5,6 +5,10 @@ import com.decagon.OakLandv1be.config.jwt.RSAKeyProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
--- src/main/java/com/decagon/OakLandv1be/controllers/AuthenticationController.java
+++ src/main/java/com/decagon/OakLandv1be/controllers/AuthenticationController.java
@@ -12,6 +12,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
 import javax.validation.Valid;
