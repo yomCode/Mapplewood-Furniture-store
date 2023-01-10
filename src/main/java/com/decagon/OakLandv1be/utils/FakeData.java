defc7bcaeffa71168a12c14363d29ec434295425
rich8rds <rbukunmi8@gmail.com>
2023-01-10 20:26:51 +0100

    Edited FakeData.java class

--- src/main/java/com/decagon/OakLandv1be/utils/FakeData.java
+++ src/main/java/com/decagon/OakLandv1be/utils/FakeData.java
@@ -1,9 +1,10 @@
 package com.decagon.OakLandv1be.utils;
 
-import com.decagon.OakLandv1be.entities.Person;
-import com.decagon.OakLandv1be.entities.Product;
+import com.decagon.OakLandv1be.entities.*;
+import com.decagon.OakLandv1be.enums.BaseCurrency;
 import com.decagon.OakLandv1be.enums.Gender;
 import com.decagon.OakLandv1be.enums.Role;
+import com.decagon.OakLandv1be.repositries.CustomerRepository;
 import com.decagon.OakLandv1be.repositries.PersonRepository;
 import com.decagon.OakLandv1be.repositries.ProductRepository;
 import lombok.RequiredArgsConstructor;
@@ -17,10 +18,10 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 public class FakeData {
     private final PasswordEncoder passwordEncoder;
 
-    @Bean
+
     public CommandLineRunner commandLineRunner(PersonRepository personRepository, ProductRepository productRepository) {
         return argument -> {
-            if(!personRepository.existsByEmail("benson@gmail.com")) {
+            if (!personRepository.existsByEmail("benson@gmail.com")) {
                 Person person = Person.builder()
                         .firstName("Benson")
                         .lastName("Malik")
@@ -36,7 +37,7 @@ public class FakeData {
                 personRepository.save(person);
             }
 
-            if(productRepository.existsById(1L)) {
+            if (productRepository.existsById(1L)) {
                 Product product = Product.builder()
                         .name("Oppola")
                         .price(40000.00)
@@ -52,8 +53,8 @@ public class FakeData {
         };
     }
 
-    @Bean
-    public CommandLineRunner commandLineRunner(PersonRepository personRepository,CustomerRepository customerRepository) {
+
+    public CommandLineRunner commandLineRunner(PersonRepository personRepository, CustomerRepository customerRepository) {
         return args -> {
             Person person = Person.builder()
                     .firstName("Maggie")
@@ -80,4 +81,6 @@ public class FakeData {
 
             personRepository.save(person);
             customerRepository.save(customer);
+        };
+    }
 }
