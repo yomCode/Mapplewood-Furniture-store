376fbf2afaf6fb3a22203b81f365ceef8665a807
Abayomi Mustapha <107572498+yomCode@users.noreply.github.com>
2023-01-14 21:12:01 +0100

    Mockito test written

--- src/test/java/com/decagon/OakLandv1be/services/serviceImpl/WalletServiceImplTest.java
+++ src/test/java/com/decagon/OakLandv1be/services/serviceImpl/WalletServiceImplTest.java
@@ -1,4 +1,3 @@
-
 package com.decagon.OakLandv1be.services.serviceImpl;
 
 import com.decagon.OakLandv1be.dto.FundWalletRequest;
@@ -7,97 +6,87 @@ import com.decagon.OakLandv1be.entities.Customer;
 import com.decagon.OakLandv1be.entities.Person;
 import com.decagon.OakLandv1be.entities.Transaction;
 import com.decagon.OakLandv1be.entities.Wallet;
-import com.decagon.OakLandv1be.enums.BaseCurrency;
-import com.decagon.OakLandv1be.enums.Gender;
-import com.decagon.OakLandv1be.enums.Role;
+import com.decagon.OakLandv1be.exceptions.UnauthorizedUserException;
 import com.decagon.OakLandv1be.repositries.PersonRepository;
 import com.decagon.OakLandv1be.repositries.TransactionRepository;
 import com.decagon.OakLandv1be.repositries.WalletRepository;
-import com.decagon.OakLandv1be.services.JavaMailService;
-import com.decagon.OakLandv1be.services.WalletService;
-import com.decagon.OakLandv1be.utils.ResponseManager;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.Mockito;
-import org.mockito.MockitoAnnotations;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 
 import java.io.IOException;
 import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyDouble;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
-//@SpringBootTest
 @ExtendWith(MockitoExtension.class)
-class WalletServiceImplTest {
-
-
+public class WalletServiceImplTest {
 
     @Mock
     private PersonRepository personRepository;
+
     @Mock
     private WalletRepository walletRepository;
+
     @Mock
     private TransactionRepository transactionRepository;
-    @Mock
-    private ResponseManager responseManager;
 
     @Mock
-    private JavaMailService mailService;
-
-//    @Mock
-    Person person;
+    private JavaMailServiceImpl mailService;
 
     @InjectMocks
     private WalletServiceImpl walletService;
 
-    @BeforeEach
-    void setUp() {
-        MockitoAnnotations.openMocks(this);
+    @Test
+    void testFundWallet_success() throws IOException {
+        // Arrange
+        FundWalletRequest request = new FundWalletRequest(100.0);
+        Authentication authentication = mock(Authentication.class);
+        when(authentication.getName()).thenReturn("test@example.com");
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+
+        Person person = new Person();
+        person.setEmail("test@example.com");
+        person.setFirstName("Test");
+        person.setLastName("User");
+        when(personRepository.findByEmail("test@example.com")).thenReturn(Optional.of(person));
 
         Wallet wallet = new Wallet();
-        Customer customer = new Customer();
-        wallet.setAccountBalance(0.00);
-        wallet.setCustomer(customer);
-        wallet.setBaseCurrency(BaseCurrency.DOLLAR);
+        wallet.setAccountBalance(1000.0);
+        when(walletRepository.save(any(Wallet.class))).thenAnswer(i -> i.getArguments()[0]);
+        person.setCustomer(new Customer());
+        person.getCustomer().setWallet(wallet);
 
-        person = Person.builder()
-                .customer(customer)
-                .email("abc@gmail.com")
-                .address("lagos")
-                .gender(Gender.MALE)
-                .phone("1234567890")
-                .date_of_birth("10/10/1990")
-                .firstName("Yomi")
-                .lastName("Musty")
-                .password("123456789")
-                .verificationStatus(true)
-                .role(Role.CUSTOMER)
-                .build();
+        when(transactionRepository.save(any(Transaction.class))).thenAnswer(i -> i.getArguments()[0]);
 
-        when(personRepository.findByEmail(any())).thenReturn(Optional.of(person));
+        // Act
+        FundWalletResponseDto response = walletService.fundWallet(request);
 
-
+        // Assert
+        assertEquals(1100.0, response.getNewBalance());
+        assertEquals(100.0, response.getDepositAmount());
+        assertEquals("Test User", response.getFullName());
+        verify(walletRepository, times(1)).save(any(Wallet.class));
+        verify(transactionRepository, times(1)).save(any(Transaction.class));
+        verify(mailService, times(1)).sendMail(eq("test@example.com"), eq("Wallet deposit"), anyString());
     }
 
     @Test
-    void fundWallet() {
-        FundWalletRequest request = new FundWalletRequest(10000.00);
-        FundWalletResponseDto response = FundWalletResponseDto.builder().fullName(person.getFirstName() + " " + person.getLastName())
-                .depositAmount(request.getAmount())
-                        .newBalance(person.getCustomer().getWallet().getAccountBalance()).build();
-//        walletService.fundWallet(request);
-        when(walletService.fundWallet(request)).thenReturn(response);
+    void testFundWallet_unauthorized() {
+        // Arrange
+        FundWalletRequest request = new FundWalletRequest(100.0);
+        Authentication authentication = mock(AnonymousAuthenticationToken.class);
+        SecurityContextHolder.getContext().setAuthentication(authentication);
 
-        assertEquals(response, walletService.fundWallet(request));
-
+        // Act and assert
+        assertThrows(UnauthorizedUserException.class, () -> walletService.fundWallet(request));
     }
 }
